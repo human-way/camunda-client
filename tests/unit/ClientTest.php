f14d12e8ef262f4ca864879fce3a01cbbf9256da
Temir Umurzakov <temir@umurzakov.com>
2021-06-24 22:49:15 +0600

    Simple request performed

--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -1,5 +1,13 @@
 <?php
 
+namespace HumanWay\Tests\unit;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+
 class ClientTest extends \Codeception\Test\Unit
 {
     /**
@@ -21,4 +29,33 @@ class ClientTest extends \Codeception\Test\Unit
         $this->assertTrue(class_exists('\HumanWay\CamundaClient'));
     }
 
+    public function testShouldMakeRequest() {
+
+        $httpClient = $this->getHttpClientMock(['status'=>'ok'], 200);
+
+        $client = new \HumanWay\CamundaClient(
+            [
+                'url'=>'http://localhost/engine-rest'
+            ],
+            $httpClient
+        );
+
+        $tasks = $client->request(\HumanWay\CamundaClient::GET, ['tasks']);
+        $this->assertNotEmpty($tasks);
+    }
+
+    public function getHttpClientMock($responseData, $statusCode) : ClientInterface {
+        $headers = ['Content-Type' => 'application/json'];
+        $body = json_encode($responseData);
+
+        $response = new Response($statusCode, $headers, $body);
+
+        $mock = new MockHandler([
+            $response
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+        return $client;
+    }
 }
